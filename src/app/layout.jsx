d1da2b671ae7202b5cd2635ff912c7c0976a5302
HieuLV03 <levanhieu14102k3@gmail.com>
2026-05-18 21:14:25 +0700

    u

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,21 +4,18 @@ import Header from "@/components/Layout/Header/Header";
 export const metadata = {
   title: "THẨM MỸ VIỆN HISU",
   description: "Thẩm mỹ công nghệ cao",
+
+  verification: {
+    google: "iMhkqfnYHYPZ2e7ZhvNa8URs0nteVRjtS03F9CKa0sU",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <body className="appBody">
-
-        {/* HEADER */}
         <Header />
-
-        {/* PAGE CONTENT */}
-        <main className="mainContent">
-          {children}
-        </main>
-
+        <main className="mainContent">{children}</main>
       </body>
     </html>
   );
