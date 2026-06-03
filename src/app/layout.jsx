00b853b1c4d1d927dc4937ab42b07bfc6027944e
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-03 16:45:59 +0700

    fixx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,29 @@
 import "./globals.css";
+
+import { Inter, Playfair_Display,  Poppins } from "next/font/google";
+
 import Header from "@/components/Layout/Header/Header";
 import Footer from "@/components/Layout/Footer/Footer";
 import BookingPopup from "@/components/BookingPopup/BookingPopup";
 import ZaloButton from "@/components/ZaloButton/ZaloButton";
 import BookingButton from "@/components/BookingButton/BookingButton";
-
+const inter = Inter({
+  subsets: ["latin", "vietnamese"],
+  display: "swap",
+  variable: "--font-inter",
+});
+const poppins = Poppins({
+  subsets: ["latin", "latin-ext"], // ✅ Đúng
+  weight: ["400", "500", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["500", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-playfair",
+});
 export const metadata = {
   metadataBase: new URL(
     "https://thammyvienhisu.online"
@@ -30,12 +49,18 @@ export const metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="vi">
+<html
+  lang="vi"
+  className={`
+    ${inter.variable}
+    ${playfair.variable}
+    ${poppins.variable}
+  `}
+>
       <body className="appBody">
         <Header />
         <main className="mainContent">{children}</main>
