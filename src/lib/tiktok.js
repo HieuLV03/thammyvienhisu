5564a8eff18415efcf6f3487231604fe4bdfe609
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-04 14:08:32 +0700

    f

--- src/lib/tiktok.js
+++ src/lib/tiktok.js
@@ -26,7 +26,7 @@ export const initTikTokPixel = () => {
       document.head.appendChild(s);
     };
 
-    ttq.load("D8GH643C77UDLID670L0");
+    ttq.load("D8GI7M3C77UDLID672C0");
     ttq.page();
   }(window, document, 'ttq');
 };
