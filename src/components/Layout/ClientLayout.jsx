ec35083fce18af07cc8d3cc4255db70169a3f567
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-04 13:18:18 +0700

    uuui

--- src/components/Layout/ClientLayout.jsx
+++ src/components/Layout/ClientLayout.jsx
@@ -3,8 +3,8 @@
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Layout/Header/Header";
+import Footer from "@/components//LayoutFooter/Footer";
 import ZaloButton from "@/components/ZaloButton/ZaloButton";
 import BookingButton from "@/components/BookingButton/BookingButton";
 import BookingPopup from "@/components/BookingPopup/BookingPopup";
